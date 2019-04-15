0be264711f60ee693824ffcb03073f40c6f7fc6a
megatkhs <tkhsryodraw@gmail.com>
2019-04-15 13:31:32 +0900

    [v1.1.1]コードをリファクタリング

--- dist/jquery-scrollable-table.js
+++ dist/jquery-scrollable-table.js
@@ -1,10 +1,16 @@
 (function($) {
+  // 初期状態のテーブルを格納
   var $original;
-  var $table, $parent, $thead, $thead_th, $thead_first_th, $thead_last_th, $tbody, $tbody_tr, $tbody_first_th;
-  var $container, $viewport;
+  // DOM操作に必要な要素
+  var $table, $parent, $thead, $thead_th, $thead_first_th, $tbody, $tbody_tr, $tbody_first_th;
+  // テーブルの可視範囲を定義する要素（CSSで定義する）
+  var $viewport;
   var tbody_first_th_width;
+  // 形成後の列の幅を格納
   var row_width;
+  // レスポンシブ対応のフラグ
   var is_pc;
+  // 初期設定
   var config = {
     el_width: 200,
     sp_el_width: 300,
@@ -16,28 +22,35 @@
   };
 
   function init($el, options) {
+    // オプションが渡されていなかったら空のオブジェクトを格納
     options = options ? options : {};
+    // テーブルのクローンを保存
     $original = $el.clone(true);
+
+    // その他の要素を各変数に保存
     $table = $el;
     $parent = $table.parent();
     $thead = $('thead', $table);
     $thead_th = $('tr th', $thead);
     $thead_first_th = $('tr th:first-child', $thead);
-    $thead_last_th = $('tr th:last-child', $thead);
     $tbody = $('tbody', $table);
     $tbody_tr = $('tr', $tbody);
     $tbody_first_th = $('tr th:first-child', $tbody);
 
+    // viewportを作成
     $viewport = $('<div class="table-viewport"></div>');
 
+    // 設定のkeyを配列に
     var config_map = Object.keys(config);
 
+    // optionsに該当のキーがあれば設定を上書き
     for (var i = 0; i < config_map.length; i++) {
       if (options[config_map[i]]) {
         config[config_map[i]] = options[config_map[i]];
       }
     }
 
+    // option.brakepointが設定してあった場合画面幅からフラグを設定
     if (config.breakpoint && window.innerWidth <= config.breakpoint) {
       is_pc = false;
       row_width = config.sp_el_width;
@@ -46,58 +59,66 @@
       row_width = config.el_width;
     }
 
+    // option.brakepointが設定してあった場合リサイズイベントを講読
     if (config.breakpoint) {
       $(window).on('resize', resize);
     }
 
+    // scrollable-tableを作成し置き換え
     renderElement();
+
+    // $viewport要素のスクロールイベントを講読
     $viewport.on('scroll', tableScroll);
   }
 
+  // scrollable-tableに置き換える処理
   function renderElement() {
+    // 左上端の要素を除いたthead > tr > thの個数
     var thead_th_length = $('tr th:not(:first-child)', $thead).length;
+
+    // tableに横幅と、左端の余白を消すスタイルを指定
     $table.css({
       'width': row_width * (thead_th_length + 1) + 'px',
       'margin-left': - row_width + 'px'
     });
 
-    var subject_width = $('tbody tr th:first-child', $table).outerWidth();
-    $thead_first_th.css({
-      'width': subject_width + 'px'
-    });
+    // 左上端の要素の横幅を取得
     tbody_first_th_width = $tbody_first_th.outerWidth();
-    tbody_th_width = $('tr th:not(:first-child)', $tbody).outerWidth();
 
+    // theadのサイズと位置ずらすスタイルを指定
     $thead.css({
       'width': row_width * thead_th_length + 'px',
       'transform': 'translate(' + row_width + 'px, -100%)'
     });
 
+    // thead > tr > thに横幅を指定
     $thead_th.css({
       width: row_width + 'px'
     })
 
+    // 左端の要素をズラすスタイルを指定
     $tbody_first_th.css('transform', 'translateX(' + (row_width - tbody_first_th_width) + 'px)');
 
+    // 左上端の要素の横幅を指定
     $thead_first_th.css('width', tbody_first_th_width + 'px');
 
-    var userAgent = navigator.userAgent.toLowerCase();
-    var isSingleAdjust = userAgent.indexOf('msie') != -1 || userAgent.indexOf('trident') > -1;
-    var adjust =  isSingleAdjust ? 1 : 2;
-    $thead_last_th.css('width', row_width - adjust + 'px');
-
+    // 各行の項目高さに合わせて項目めいの高さを指定
     $tbody_tr.each(function(index, $tr) {
       $('th:first-child', $tr).css('height', $('th:nth-child(2), td:nth-child(2)', $tr).height() + 'px');
     });
 
+    // 要素を置き換え
     $viewport.append($table);
     $parent.append($viewport);
 
-
+    // theadの高さを取得
     var thead_height = $thead.height();
+
+    // $viewportをズラすスタイルを指定
     $viewport.css({
       'margin-top': thead_height + 'px',
       'margin-left': tbody_first_th_width + 'px',
+      'width': 'calc(100% - ' + tbody_first_th_width + 'px)'
     });
   }
 
@@ -108,6 +129,7 @@
     $tbody_first_th.css('transform', 'translate(' + (row_width - tbody_first_th_width) + 'px, ' + (-event.target.scrollTop) + 'px)');
   }
 
+  // テーブルを元に戻す処理
   function restore() {
     $viewport.off('scroll');
     $(window).off('resize');
@@ -115,7 +137,8 @@
     $parent.append($original);
   }
 
-  function resize(event) {
+  // レスポンシブ用の切り替え処理
+  function resize() {
     if (is_pc && window.innerWidth <= config.breakpoint) {
       is_pc = false;
       restore()
